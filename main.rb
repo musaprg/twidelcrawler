ea5c3c1a80ee27c5d44ca7d0e0a1f4b3d8cab921
musaprg <conie112@gmail.com>
2017-01-09 01:36:03 +0900

    [modified]ツイ消し検知だけ作ったよ

--- main.rb
+++ main.rb
@@ -3,39 +3,42 @@
 require 'pg'
 require 'yaml'
 require 'twitter'
-require 'tweetstream'
+# require 'net/https'
+# require 'oauth'
+# require 'cgi'
+# require 'json'
 
 twconf = YAML.load_file("config.yml")["twitter"]
-dbconf = YAML.load_file("config.yml")["db"]["product"]
+dbconf = YAML.load_file("config.yml")["db"]["development"]
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = twconf.consumer_key
-  config.consumer_secret     = twconf.consumer_secret
-  config.access_token        = tfconf.access_token
-  config.access_token_secret = tf.conf.access_token_secret
+
+rest = Twitter::REST::Client.new do |config|
+  config.consumer_key        = twconf["consumer_key"]
+  config.consumer_secret     = twconf["consumer_secret"]
+  config.access_token        = twconf["access_token"]
+  config.access_token_secret = twconf["access_token_secret"]
+end
+stream = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = twconf["consumer_key"]
+  config.consumer_secret     = twconf["consumer_secret"]
+  config.access_token        = twconf["access_token"]
+  config.access_token_secret = twconf["access_token_secret"]
 end
 
-stream = TweetStream::Client.new do |config|
-   config.consumer_key          = twconf.consumer_key
-   config.consumer_secret       = twconf.consumer_secret
-   config.oauth_token           = twconf.access_token
-   config.oauth_token_secret    = twconf.access_token_secret
+stream.user do |status|
+  case status
+  when Twitter::Tweet then
+    # username = status.user.screen_name
+    # contents = status.text
+    # str = username + ":" + contents
+    # puts str
+  when Twitter::Streaming::DeletedTweet then
+    user = rest.user(status.user_id)
+    rest.update("#{user.name}(@#{user.screen_name})のツイ消しを検出しました。") if user.id != 817254158839332865
+  end
 end
 
 # initialize db connection
 connection = PG::connect(@dbconf)
-
-# check table exists. if not, create new one.
-begin
-    result = connection.exec("
-      SHOW TABLES LIKE 'timeline'
-    ")
-    p result
-end
-
-stream.userstream do |status|
-   puts " #{status.user.name} -> #{status.text}\n\n"
-end
-
 # close db connection
 connection.finish if @connection
